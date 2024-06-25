2cd990b928b74cff86b35ebb78ea0259896dbc52
Peter Szollar <peter@Peters-MacBook-Pro.local>
2024-06-25 11:34:49 +0100

    fixed build and home ui

--- composeApp/src/androidMain/kotlin/com/vertexcode/luxuryeye/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/vertexcode/luxuryeye/MainActivity.kt
@@ -5,13 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.vertexcode.luxuryeye.presentation.home.Home
+import com.vertexcode.luxuryeye.presentation.user.auth.EmailConfirmation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            App()
+            Home()
         }
     }
 }
